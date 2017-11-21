13079dcee03c14757d63b7183a8baf0b81244dd3
shangyukyo <sdzc1014@163.com>
2017-11-21 16:12:41 +0800

    fixbug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
 
 
 
@@ -13,7 +13,7 @@
 # ==================
 
 # Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
+# group is considered to be the first unless any hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
@@ -49,13 +49,36 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
+# server "example.com",
+#   user: "user_name",
 #   roles: %w{web app},
 #   ssh_options: {
-#     user: 'user_name', # overrides user setting above
+#     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
+#     # password: "please use keys"
 #   }
+
+set :deploy_to, '~/www/dog-training/'
+
+# server '115.29.167.37',
+#   user: 'shopshow',
+#   roles: %w{web app db},
+#   ssh_options: {
+#     user: 'shopshow', # overrides user setting above
+#     keys: %w(/home/shopshow/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password),
+#     password: 'Shopshow123456chuheridangwu'
+#   }
+
+server '47.95.223.242',
+  user: 'duole',
+  roles: %w{web app db},
+  ssh_options: {
+    user: 'duole', # overrides user setting above
+    # keys: %w(/home/ssapp/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)    
+  }
